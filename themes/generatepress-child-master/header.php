3ec7ca97e1781853d9528edea13b8554c9eef606
Santiago Mesa <development@alaronastudio.com>
2024-02-22 15:27:45 -0500

    ajustes varios al header.
    Se ha actualizado el código para que el selector de idiomas funcione correctamente.

--- themes/generatepress-child-master/header.php
+++ themes/generatepress-child-master/header.php
@@ -83,17 +83,25 @@ if (!defined('ABSPATH')) {
 							<li class="menu-item-lang">								
 								<a href="javascript:void(0);">
 									<?php echo __('Lenguaje','alarona'); ?>
-									<svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M5.29303 7.79259C5.48056 7.60512 5.73487 7.49981 6.00003 7.49981C6.26519 7.49981 6.5195 7.60512 6.70703 7.79259L10 11.0856L13.293 7.79259C13.3853 7.69708 13.4956 7.6209 13.6176 7.56849C13.7396 7.51608 13.8709 7.4885 14.0036 7.48734C14.1364 7.48619 14.2681 7.51149 14.391 7.56177C14.5139 7.61205 14.6255 7.68631 14.7194 7.7802C14.8133 7.87409 14.8876 7.98574 14.9379 8.10864C14.9881 8.23154 15.0134 8.36321 15.0123 8.49599C15.0111 8.62877 14.9835 8.75999 14.9311 8.882C14.8787 9.004 14.8025 9.11435 14.707 9.20659L10.707 13.2066C10.5195 13.3941 10.2652 13.4994 10 13.4994C9.73487 13.4994 9.48056 13.3941 9.29303 13.2066L5.29303 9.20659C5.10556 9.01907 5.00024 8.76476 5.00024 8.49959C5.00024 8.23443 5.10556 7.98012 5.29303 7.79259Z" fill="#F3F4F6"/>
-</svg>
+									<svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.29303 7.79259C5.48056 7.60512 5.73487 7.49981 6.00003 7.49981C6.26519 7.49981 6.5195 7.60512 6.70703 7.79259L10 11.0856L13.293 7.79259C13.3853 7.69708 13.4956 7.6209 13.6176 7.56849C13.7396 7.51608 13.8709 7.4885 14.0036 7.48734C14.1364 7.48619 14.2681 7.51149 14.391 7.56177C14.5139 7.61205 14.6255 7.68631 14.7194 7.7802C14.8133 7.87409 14.8876 7.98574 14.9379 8.10864C14.9881 8.23154 15.0134 8.36321 15.0123 8.49599C15.0111 8.62877 14.9835 8.75999 14.9311 8.882C14.8787 9.004 14.8025 9.11435 14.707 9.20659L10.707 13.2066C10.5195 13.3941 10.2652 13.4994 10 13.4994C9.73487 13.4994 9.48056 13.3941 9.29303 13.2066L5.29303 9.20659C5.10556 9.01907 5.00024 8.76476 5.00024 8.49959C5.00024 8.23443 5.10556 7.98012 5.29303 7.79259Z" fill="#F3F4F6"/></svg>
 								</a>
 								<ul class="submenu-lang">
 									<?php 
-									$languages = icl_get_languages('skip_missing=0');									
+									$languages = icl_get_languages('skip_missing=0');	
+									$current_language_code = icl_get_current_language();									
 
 									if (!empty($languages)) { 
 										foreach ($languages as $language) { 
-											echo '<li><a href="' . $language['url'] . '">' . $language['native_name'] . '</a></li>';											
+											
+											// <?php echo ($language['language_code'] === ICL_LANGUAGE_CODE) ? 'current-language' : '';
+
+											if ($language['language_code'] === ICL_LANGUAGE_CODE ) {
+												$currentLanguage = 'class="current-language"';
+											} else {
+												$currentLanguage = '';
+											}
+
+											echo '<li '. $currentLanguage .'><a href="' . $language['url'] . '">' . $language['native_name'] . '</a></li>';											
 										}
 									}
 									?>
@@ -102,6 +110,9 @@ if (!defined('ABSPATH')) {
 							</li>
 						</ul>
 					</nav>
+
+
+
 				</div>
 			</div>
 		</div>
